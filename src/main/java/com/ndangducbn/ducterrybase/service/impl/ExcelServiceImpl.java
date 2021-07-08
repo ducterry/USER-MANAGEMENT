6ab8fd145dd915ace79c4b492446472d55fd611e
Nguyen Dang Duc <dev.ndangduc.bn@gmail.com>
2021-07-08 09:07:45 +0700

    Export Action => Download Templates  => Cherry to DEV

--- src/main/java/com/ndangducbn/ducterrybase/service/impl/ExcelServiceImpl.java
+++ src/main/java/com/ndangducbn/ducterrybase/service/impl/ExcelServiceImpl.java
@@ -8,7 +8,7 @@ import com.ndangducbn.ducterrybase.enums.ETypeDateFormat;
 import com.ndangducbn.ducterrybase.helper.Convert2DTO;
 import com.ndangducbn.ducterrybase.model.response.ExcelUsersDTO;
 import com.ndangducbn.ducterrybase.model.response.UserResDTO;
-import com.ndangducbn.ducterrybase.service.ExportService;
+import com.ndangducbn.ducterrybase.service.ExcelService;
 import com.ndangducbn.ducterrybase.utils.DateUtil;
 import com.ndangducbn.ducterrybase.utils.FileUtil;
 import com.ndangducbn.ducterrybase.utils.JSONFactory;
@@ -28,14 +28,14 @@ import java.util.List;
 
 @Service
 @Slf4j
-public class ExportServiceImpl implements ExportService {
+public class ExcelServiceImpl implements ExcelService {
     private final String PREFIX = "ExcelController_";
 
     private final Convert2DTO convert2DTO;
     private final UserRepository userRepository;
     private final ModelMapper modelMapper;
 
-    public ExportServiceImpl(Convert2DTO convert2DTO, UserRepository userRepository, ModelMapper modelMapper) {
+    public ExcelServiceImpl(Convert2DTO convert2DTO, UserRepository userRepository, ModelMapper modelMapper) {
         this.convert2DTO = convert2DTO;
         this.userRepository = userRepository;
         this.modelMapper = modelMapper;
@@ -49,7 +49,7 @@ public class ExportServiceImpl implements ExportService {
             /*
                 01. Tạo thư mục Export File
              */
-            String path = PathConstant.EXCEL_PATH_SAVED;
+            String path = PathConstant.EXCEL_PATH_EXPORT_SAVED;
             File dir = new File(path);
             if (!dir.exists()) {
                 dir.mkdirs();
@@ -61,7 +61,7 @@ public class ExportServiceImpl implements ExportService {
             XSSFWorkbook workbook = null;
             InputStream excelFile;
             try {
-                path = path +"/"+ DateUtil.dateToString(ETypeDateFormat.DATE_FORMAT_FULL, new Date()) + ".xlsx";
+                path = path +"/DANH_SACH_TAI_KHOAN"+ DateUtil.dateToString(ETypeDateFormat.DATE_FORMAT_FULL, new Date()) + ".xlsx";
                 excelFile = new ClassPathResource(PathConstant.EXCEL_EXPORT).getInputStream();
                 workbook = new XSSFWorkbook(excelFile);
             } catch (Exception e) {
@@ -112,6 +112,51 @@ public class ExportServiceImpl implements ExportService {
 
             /*
                 05. Export File
+             */
+            try (FileOutputStream outputStream = new FileOutputStream(path)) {
+                workbook.write(outputStream);
+                outputStream.flush();
+            } catch (FileNotFoundException e) {
+                log.error(e.getMessage(), e);
+            } catch (IOException e) {
+                log.error(e.getMessage(), e);
+                return "";
+            }
+            return path;
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    @Override
+    public String downloadTemplates() {
+        try {
+            log.debug(PREFIX + "downloadTemplates =>");
+
+            /*
+                01. Tạo thư mục Export File
+             */
+            String path = PathConstant.EXCEL_PATH_DOWNLOAD_SAVED;
+            File dir = new File(path);
+            if (!dir.exists()) {
+                dir.mkdirs();
+            }
+
+            /*
+                02. Xử lí Workbook
+             */
+            XSSFWorkbook workbook = null;
+            InputStream excelFile;
+            try {
+                path = path +"/DANH_SACH_TAI_KHOAN"+ DateUtil.dateToString(ETypeDateFormat.DATE_FORMAT_FULL, new Date()) + ".xlsx";
+                excelFile = new ClassPathResource(PathConstant.EXCEL_DOWNLOAD_TEMPLATES).getInputStream();
+                workbook = new XSSFWorkbook(excelFile);
+            } catch (Exception e) {
+                log.error(PREFIX + "Export Excel Exception => {}", JSONFactory.toString(e.getMessage()));
+            }
+
+            /*
+                03. Export File
              */
             try (FileOutputStream outputStream = new FileOutputStream(path)) {
                 workbook.write(outputStream);
